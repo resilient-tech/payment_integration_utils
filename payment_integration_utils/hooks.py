c498c01ac60e7ea45172df6500c80d78b36d50eb
Abdeali Chharchhoda <abdealiking786@gmail.com>
2025-02-13 12:03:24 +0530

    feat: Update app license to GNU GPL v3 and specify required apps

--- payment_integration_utils/hooks.py
+++ payment_integration_utils/hooks.py
@@ -3,4 +3,5 @@ app_title = "Payment Integration Utils"
 app_publisher = "Resilient Tech"
 app_description = "Base for integrate online payment integrations"
 app_email = "info@resilient.tech"
-app_license = "mit"
+app_license = "GNU General Public License (v3)"
+required_apps = ["frappe/erpnext"]
