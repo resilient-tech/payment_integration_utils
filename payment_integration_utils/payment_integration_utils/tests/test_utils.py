f0f0409dcadbff173c59bd45b591eb57079eaf15
Abdeali Chharchhoda <abdealiking786@gmail.com>
2025-02-25 13:27:15 +0530

    test: Add unit tests for currency conversion functions

--- payment_integration_utils/payment_integration_utils/tests/test_utils.py
+++ payment_integration_utils/payment_integration_utils/tests/test_utils.py
@@ -4,8 +4,16 @@ from payment_integration_utils.payment_integration_utils.utils import *
 
 
 class TestUtils(FrappeTestCase):
-    def test_start_of_day_epoch(self):
-        self.assertEqual(get_start_of_day_epoch("2024-05-30"), 1717007400)
-
-    def test_end_of_day_epoch(self):
-        self.assertEqual(get_end_of_day_epoch("2024-05-30"), 1717093799)
+    def test_conversion(self):
+        # (100, 10000) => 100 Rupees = 10000 Paisa
+        data = [
+            (100, 10000),
+            (100.0, 10000),
+            (100.5, 10050),
+            (0.5, 50),
+            (79.9, 7990),
+            (0, 0),
+        ]
+        for rupees, paisa in data:
+            self.assertEqual(rupees_to_paisa(rupees), paisa)
+            self.assertEqual(paisa_to_rupees(paisa), rupees)
